ed137ad192131f3ee19c82b21b41ac0336406ad4
anilyavas <anil.yavas99@gmail.com>
2024-12-14 21:08:51 +0300

    fetchTasks function and creating db

---
+++ functions/fetchTasks.ts
@@ -0,0 +1,23 @@
+import { supabase } from '@/utils/supabase';
+
+const fetchTasks = async () => {
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) throw new Error('User not logged in');
+
+    const { data, error } = await supabase
+      .from('tasks')
+      .select('*')
+      .eq('user_id', user?.id);
+
+    if (error) {
+      console.log(error);
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error('Failed while fetching tasks');
+  }
+};
