b8b2a949f0f3c6a623f4fd182b01f5de185686b8
anilyavas <anil.yavas99@gmail.com>
2024-12-15 12:46:41 +0300

    title and category UI

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,7 +1,7 @@
 import { createTasks } from '@/functions/fetchTasks';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
-import { Alert, View, Text } from 'react-native';
+import { Alert, View, Text, Pressable, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Create() {
@@ -35,9 +35,46 @@ export default function Create() {
   };
 
   return (
-    <View className='bg-slate-300 flex-1'>
+    <View className='bg-slate-200 flex-1'>
       <SafeAreaView>
         <Text className='text-bold text-2xl text-center'>Create Task</Text>
+        <View>
+          <Text className='font-semibold text-2xl pl-4 pt-4'>Task Title</Text>
+          <TextInput
+            className='border border-gray-400 p-3 m-4'
+            value={title}
+            onChangeText={setTitle}
+            placeholder='Title'
+          />
+        </View>
+        <View className='pt-2 p-2'>
+          <Text className='font-semibold text-xl pl-2'>Category</Text>
+          <View className='flex-row p-2 justify-around gap-1'>
+            <Pressable className={`bg-purple-600 p-4 rounded-xl w-1/3`}>
+              <Text className='text-white font-semibold text-center'>
+                Daily
+              </Text>
+            </Pressable>
+            <Pressable className={`bg-purple-400 p-4 rounded-xl w-1/3`}>
+              <Text className='font-semibold text-white text-center'>Work</Text>
+            </Pressable>
+            <Pressable className={`bg-purple-400 p-4 rounded-xl w-1/3`}>
+              <Text className='font-semibold text-white text-center'>
+                Groceries
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+        <View className='items-center p-3'>
+          <Pressable
+            className='bg-purple-600 p-4 rounded-lg w-full'
+            onPress={handleCreate}
+          >
+            <Text className='text-white font-semibold text-center text-xl'>
+              Create Task
+            </Text>
+          </Pressable>
+        </View>
       </SafeAreaView>
     </View>
   );
